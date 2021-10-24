9d092e7720dd882b4e7d099172c1b6eff975eb58
Dibae101 <dibaekhanal101@gmail.com>
2021-10-24 14:34:35 +0545

    dockerized

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/batman/Desktop/Projects/personalprojects/docker/CRUD/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
